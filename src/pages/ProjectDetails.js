7f1ecb844f221e31b7f2e8629b42b4c15f86953f
mr-hollowman <selvamani825@gmail.com>
2023-04-21 20:17:35 +0530

    project details page updates

--- src/pages/ProjectDetails.js
+++ src/pages/ProjectDetails.js
@@ -1,36 +1,57 @@
 import React, { useEffect, useState } from 'react'
-import { useLocation } from 'react-router-dom'
+import { useLocation, useParams } from 'react-router-dom'
+import axios from 'axios'
 
 export default function ProjectDetails() {
   const [userInfo, setUserInfo] = useState([])
-  const location = useLocation()
-  const { data } = location.state
-  console.log(data, "locationdata");
+  const [data, setData] = useState([])
+  const [isPending, setIspending] = useState([])
+  const [error, setError] = useState(false)
+  const { id } = useParams()
   useEffect(() => {
     const info = JSON.parse(localStorage.getItem('user'))
     setUserInfo(info)
+    setIspending(true)
+    axios.get("http://192.168.1.41:8080/api/projects/" + id, { params: { id: id } })
+      .then((res) => {
+        if (res.status === 200) {
+          setData(res.data)
+          setIspending(false)
+        }
+      }).catch((err) => {
+        setIspending(false)
+        setError(err.message)
+      })
   }, [])
   console.log(userInfo);
   return (
     <>
-      <div className='row m-1 text-white'>
+      {isPending && <div className="d-flex align-items-center mt-5 p-3 bg-white">
+        <strong>Loading...</strong>
+        <div className="spinner-border ms-auto" role="status" aria-hidden="true"></div>
+      </div>}
+
+      {error && <span className="p-5 d-flex justify-content-center">{error}</span>}
+
+      {!isPending && data && !error && <><div className='row m-1 text-white'>
         <div className='mt-2 p-4 bg-primary d-flex justify-content-between '>
           <h3>{data.title}</h3>
           <span>Budget ₹ {data.startPrice} - {data.endPrice} INR</span>
         </div>
       </div>
-      <div className='container mt-3 p-4 bg-white'>
-        <p>Job Description:</p>
-        <p className='mx-3 text-muted'>{data.description}</p>
-        <p>Skills : <span className='text-primary'>{data.tag}</span></p>
-        <p>About the client:</p>
-        <div className='mx-3 d-flex flex-column'>
-          <span>Name: {userInfo.userName}</span>
-          <span>email Id: {userInfo.email}</span>
+        <div className='container mt-3 p-4 bg-white'>
+          <p>Job Description:</p>
+          <p className='mx-3 text-muted'>{data.description}</p>
+          <p>Skills : <span className='text-primary'>{data.tag}</span></p>
+          <p>About the client:</p>
+          <div className='mx-3 d-flex flex-column'>
+            <span>Name: {userInfo.userName}</span>
+            <span>email Id: {userInfo.email}</span>
+          </div>
+          <p className='mt-3'>Project Id: {data._id}</p>
+          <hr></hr>
         </div>
-        <p className='mt-3'>Project Id: {data._id}</p>
-        <hr></hr>
-      </div>
+      </>}
     </>
   )
 }
